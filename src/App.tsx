efd25160fe9ef8e78ad82a11cf575632e85246de
unknown <patiladi3119@gmail.com>
2023-10-18 23:51:44 +0530

    refactored genre hooks

--- src/App.tsx
+++ src/App.tsx
@@ -10,8 +10,8 @@ import SortSelector from "./components/SortSelector";
 import GameHeading from "./components/GameHeading";
 
 export interface GameQuery {
-  genre: Genre | null;
-  platform: Platform | null;
+  genreId?: number;
+  platformId?: number;
   sortOrder: string;
   searchText: string;
 }
@@ -40,10 +40,12 @@ function App() {
         <Show above="lg">
           {" "}
           {/**The Show component will only show this GridItem when screen size is above lg */}
-          <GridItem area="aside" paddingX={5}>
+          <GridItem area="a side" paddingX={5}>
             <GenreList
-              selectedGenre={gameQuery.genre}
-              onSelectGenre={(genre) => setGameQuery({ ...gameQuery, genre })}
+              selectedGenreId={gameQuery.genreId}
+              onSelectGenre={(genre) =>
+                setGameQuery({ ...gameQuery, genreId: genre.id })
+              }
             ></GenreList>
           </GridItem>
         </Show>
@@ -53,9 +55,9 @@ function App() {
             <Flex marginBottom={5}>
               <Box marginRight={5}>
                 <PlatformSelector
-                  selectedPlatform={gameQuery.platform}
+                  selectedPlatformId={gameQuery.platformId}
                   onSelectPlatform={(platform) =>
-                    setGameQuery({ ...gameQuery, platform })
+                    setGameQuery({ ...gameQuery, platformId: platform.id })
                   }
                 ></PlatformSelector>
               </Box>
